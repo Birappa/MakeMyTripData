f8331a34eb916c3ef2453a8a4c267b540b152f91
Birappa <patilbirappa0901@gmail.com>
2019-01-24 18:29:21 +0530

    user database service

---
+++ src/main/java/com/makemytrip/data/MakeMyTripDataApplication.java
@@ -0,0 +1,20 @@
+package com.makemytrip.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MakeMyTripDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeMyTripDataApplication.class, args);
+	}
+
+	
+}
+
---
+++ src/main/java/com/makemytrip/data/controller/UserController.java
@@ -0,0 +1,73 @@
+package com.makemytrip.data.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.makemytrip.data.model.User;
+
+
+@RestController
+//@RequestMapping("/user")
+//@CrossOrigin("http://localhost:4200")
+public class UserController {
+
+
+	@Autowired
+	private MongoTemplate mongoTemplate;
+
+	@GetMapping("/users")
+	public List<User> getAllUsers(){
+		
+		return mongoTemplate.findAll(User.class);
+	}
+
+
+	@GetMapping("/users/{email}")
+	public User getUserByEmail(@PathVariable String email) {
+		
+		return mongoTemplate.findOne(new Query(Criteria.where("email").is(email)), User.class);
+	}
+
+	@PostMapping("/users/{id}")
+	public User getUserById(@PathVariable long id) {
+
+		return mongoTemplate.findById(id, User.class);
+	}
+
+	@PostMapping("/users")
+	public User saveUser(@RequestBody User user) {
+		
+		return mongoTemplate.save(user);
+		
+	}
+
+
+	@PutMapping("/users/{id}")
+	public User updateUser(@PathVariable String id,@RequestBody User user) {
+		
+		return mongoTemplate.save(user);
+	}
+
+
+	@DeleteMapping("/users/{id}")
+	public String deleteUser(@PathVariable long id) {
+		
+		mongoTemplate.remove(new Query(Criteria.where("id").is(id)), User.class);
+		return "user has deleted";
+	}
+
+
+	
+}
